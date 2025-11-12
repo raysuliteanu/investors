555f43eb727c0fe00130b74fe73a3e52494a0478
Ray Suliteanu <raysuliteanu@gmail.com>
2025-11-12 12:20:40 -0700

    make async with tokio

--- src/av.rs
+++ src/av.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Error};
 
-use reqwest::blocking::Client;
+use reqwest::Client;
 
 pub struct AV {
     client: Client,
@@ -18,13 +18,15 @@ impl AV {
         }
     }
 
-    pub fn quote(&self, symbol: &str) -> Result<String, Error> {
+    pub async fn quote(&self, symbol: &str) -> Result<String, Error> {
         let params = [("function", "GLOBAL_QUOTE"), ("symbol", symbol)];
         self.client
             .get(self.api_url.as_str())
             .query(&params)
-            .send()?
+            .send()
+            .await?
             .text()
+            .await
             .with_context(|| format!("Failed to get quote data for {}", symbol))
     }
 }
@@ -33,11 +35,11 @@ impl AV {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_quote() {
+    #[tokio::test]
+    async fn test_quote() {
         let key = std::env::var("ALPHAVANTAGE_API_KEY").expect("ALPHAVANTAGE_API_KEY not set");
         let av = AV::new(key);
-        let result = av.quote("IBM");
+        let result = av.quote("IBM").await;
         assert!(result.is_ok());
         println!("Quote data: {}", result.unwrap());
     }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ struct Cli {
     symbol: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     // TODO: if key not on cmd line and not in env, then notify user and exit
     let key = args
@@ -16,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap_or_else(|| std::env::var("ALPHAVANTAGE_API_KEY").unwrap());
 
     let av = AV::new(key);
-    let quote = av.quote(&args.symbol)?;
+    let quote = av.quote(&args.symbol).await?;
     println!("{quote}");
     Ok(())
 }
